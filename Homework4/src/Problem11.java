d87db045a32ca8de89f274072dc6b77d0631f3bf
Arsen <arsen.gh@gmail.com>
2020-02-29 16:37:34 +0400

    correct problem11

--- Homework4/src/Problem11.java
+++ Homework4/src/Problem11.java
@@ -6,13 +6,15 @@ public class Problem11 {
         System.out.print("Enter your number: ");
         int n = scanner.nextInt();
         int i = 2;
-        while (i<=n/2)
+        while (i<n)
             if (n%i==0)
             {
                 System.out.println(n+" is not prime number.");
                 break;
             }
             else i++;
-        if (n%i!=0) System.out.println(n+" is prime number.");
+        if (n==2) System.out.println(n+" is not prime number.");
+        else if (n==i) System.out.println(n+" is prime number.");
+        else if (n==1) System.out.println(n+" is prime number.");
     }
 }
