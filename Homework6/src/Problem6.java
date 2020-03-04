708e23fc0fcde8ae9d9be238d0a22f87da6d3e3d
Arsen <arsen.gh@gmail.com>
2020-03-05 00:22:35 +0400

    correct problem 6

--- Homework6/src/Problem6.java
+++ Homework6/src/Problem6.java
@@ -8,27 +8,33 @@ public class Problem6 {
         System.out.print("Input parenthesis sequence: ");
         String seq = scanner.nextLine();
         char[] arr = seq.toCharArray();
-        if (seq.length() % 2 != 0) System.out.println("Invalid");
-        else if (arr[0] != '(' || arr[arr.length - 1] != ')') System.out.println("Invalid");
-        else {
             int count1 = 0;
             int count2 = 0;
-            for (int i = 0; i < arr.length - 1; i++) {
-                if (arr[i] == '(' && arr[i + 1] == ')')
-                { count1++;count2++; }
-                else if (arr[i] == '(' && arr[i + 1] == '(')
-                count1++;
-                else if (arr[i] == ')' && arr[i + 1] == '(')
-                { count1++;count2++; }
-                else if (arr[i] == ')' && arr[i + 1] == ')')
-                count2++;
-                else break;
+            boolean match = false;
+            for (int i = 0; i < arr.length; i++) {
+                if (arr[i]=='(')
+                {
+                    count1++;
+                }
+                else if (arr[i]==')')
+                if(count1==0||count1<=count2)
+                {
+                    match = false;
+                    break;
+                }
+                else
+                {
+                    match = true;
+                    count2++;
+                }
+                else {
+                    break;
+                }
             }
-            if (count1==count2) System.out.println("Valid");
+            if (count1==count2&&match) System.out.println("Valid");
             else System.out.println("Invalid");
         }
-        }
+        } 
     }
-}
 
 
